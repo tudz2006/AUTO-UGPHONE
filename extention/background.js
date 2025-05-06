a31435a4f65db87811d688bb16132db54cdda554
tudz2006 <132770604+tudz2006@users.noreply.github.com>
2025-05-06 17:53:39 +0700

    Delete extention directory

--- extention/background.js
+++
@@ -1,8 +0,0 @@
-// background.js
-chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === 'install') {
-      // Khi extension được cài đặt lần đầu
-      chrome.tabs.create({ url: 'https://www.youtube.com/@androidmodvip' });
-    }
-  });
-  
